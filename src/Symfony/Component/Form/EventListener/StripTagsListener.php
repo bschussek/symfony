afa73d70b5bce58b4b2cf8b2f934bd79a4e67053
Miha Vrhovnik <miha.vrhovnik@pagein.si>
2011-03-26 19:05:13 +0100

    filterBoundDataFromClient -> filterBoundClientData

--- src/Symfony/Component/Form/EventListener/StripTagsListener.php
+++ src/Symfony/Component/Form/EventListener/StripTagsListener.php
@@ -29,6 +29,6 @@ class StripTagsListener implements EventSubscriberInterface
 
     public static function getSubscribedEvents()
     {
-        return Events::filterBoundDataFromClient;
+        return Events::filterBoundClientData;
     }
 }
